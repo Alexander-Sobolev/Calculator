5b2480429f62b1434b191ad9f09d8a01f46241fd
Alexander Sobolev <sobolev.ios.developer@gmail.com>
2021-01-04 22:49:37 +0300

    Initial commit

---
+++ Calculator.py
@@ -0,0 +1,18 @@
+# Calculator v.1.0
+
+what = input("Что делаем? (+, -): ")
+
+a = float(input("Введи первое число: "))
+b = float(input("Введи второе число: "))
+
+if what == "+":
+    c = a + b
+    print("Результат: " + str(c))
+elif what == "-":
+    c = a - b
+    print("Результат: " + str(c))
+else:
+    print("Выбрана неверная операция!")
+
+
+
